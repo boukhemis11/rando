72a789a825122c9fc957248802e9dd1598afb7b6
boukhemis11 <boukhemis2011@gmail.com>
2018-05-21 13:05:06 +0200

    add service

--- ngRando/src/app/app.module.ts
+++ ngRando/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HikeComponent } from './hike/hike.component';
+import { Hike } from './shared/hike';
+import { HikeService } from './hike/hike.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HikeComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [HikeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
